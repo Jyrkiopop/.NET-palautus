2b4591da5620b64c4c2f86a4af9271456285ea84
Lenni Vilminko <s2401273@edu.bc.fi>
2026-08-11 10:29:38 +0300

    starfield

---
+++ starfield/Program.cs
@@ -0,0 +1,49 @@
+﻿using Raylib_cs;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+namespace starfield
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 600, "starfield");
+            Star[] stars = new Star[400];
+            Raylib.SetTargetFPS(60);
+            Random rng = new Random();
+            Color[] palette = new Color[]
+            {
+                Color.Red,
+                Color.Purple,
+                Color.Yellow
+            };
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = new Star();
+                stars[i].position.X = rng.Next(-5,Raylib.GetScreenWidth());
+                stars[i].position.Y = rng.Next(-5, Raylib.GetScreenWidth());
+            }
+            {
+                while (!Raylib.WindowShouldClose())
+                {
+
+                    Raylib.BeginDrawing();
+                    Raylib.ClearBackground(Color.Black);
+                    for (int i = 0; i < stars.Length; i++)
+                    {
+                        stars[i] += 200 * Raylib.GetFrameTime();
+
+                        if (stars[i] > Raylib.GetScreenWidth())
+                        {
+                            stars[i] = rng.Next(-5, Raylib.GetScreenWidth());
+                        }
+                        Raylib.DrawRectangle((int)stars[i], i*10, 10, 10, Color.Yellow);
+                    }
+                    Raylib.EndDrawing();
+                }
+            }
+        }
+    }
+}
---
+++ starfield/Star.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace starfield
+{
+    internal class Star
+    {
+        public Vector2 position; //x ja y
+        public Color color;
+        public int size;
+
+    }
+}
